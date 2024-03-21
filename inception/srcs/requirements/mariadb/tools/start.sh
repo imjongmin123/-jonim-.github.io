dc2e2a168d01949b0e21cc53bd922a37bf8d3e49
임종민 <imjongmin@imjongmin-ui-MacBookAir.local>
2024-03-21 20:04:42 +0900

    last inception

--- inception/srcs/requirements/mariadb/tools/start.sh
+++ inception/srcs/requirements/mariadb/tools/start.sh
@@ -3,15 +3,11 @@
 mysqld_safe --datadir='/var/lib/mysql' &
 until mysqladmin ping --silent; do
     echo "Waiting for MariaDB server to be available..."
-    sleep 1
+    sleep 2
 done
 
+
 mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
-mysql -e "DELETE FROM mysql.user WHERE User='';"
-mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-mysql -e "DROP DATABASE IF EXISTS test;"
-mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
-mysql -e "FLUSH PRIVILEGES;"
 mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mysql -e "SELECT 1 FROM mysql.user WHERE user = '$MYSQL_USER'" | grep -q 1 || \
 mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
